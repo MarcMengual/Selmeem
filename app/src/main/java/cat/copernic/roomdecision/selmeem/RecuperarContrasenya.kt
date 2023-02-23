a781d489c4dbcadceb41c4b90b6a774f86e3bccd
Marc Mengual Gestí <mengual.gesti.marc@alumnat.copernic.cat>
2023-02-23 19:40:35 +0100

    Projecte Inicial Amb els Xml i conexió a la base de dades

---
+++ app/src/main/java/cat/copernic/roomdecision/selmeem/RecuperarContrasenya.kt
@@ -0,0 +1,11 @@
+package cat.copernic.roomdecision.selmeem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecuperarContrasenya : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recuperar_contrasenya)
+    }
+}
